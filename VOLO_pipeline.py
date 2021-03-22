4b8f6045ea058158ecd63044b803789f34b5f07c
tonglizi <931217336@qq.com>
2021-03-22 15:53:22 +0800

    Updates

--- VOLO_pipeline.py
+++ VOLO_pipeline.py
@@ -227,7 +227,10 @@ def main():
             #     scale_factor = math.sqrt(np.sum(last_rel_LO_pose[:3, -1] ** 2) / np.sum(last_rel_VO_pose[:3, -1] ** 2))
 
             # version2.0 固定模型的尺度因子
-            scale_factor = 7
+            if args.isKitti:
+                scale_factor = 35
+            else:
+                scale_factor = 7
 
             scale_factors[j] = scale_factor
             last_rel_VO_pose = copy.deepcopy(rel_VO_pose)  # 注意深拷贝
