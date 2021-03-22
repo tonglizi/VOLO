c9378395e17b1b6490cee08b3ccaa121c28a8d18
tonglizi <931217336@qq.com>
2021-03-22 11:56:40 +0800

    Updates

--- auto_run.sh
+++ auto_run.sh
@@ -38,8 +38,8 @@ vo_models_for_mydataset=(
 
 # 包括原生模型*1和新模型*1在KITTI训练集上训练的model
 vo_models_for_kitti=(
-  "data,500epochs,epoch_size3000,b32,m0.2/06-17-04_17/"
-  "data,b16,lr0.0004/03-09-11:53"
+#  "data,500epochs,epoch_size3000,b32,m0.2/06-17-04_17/"
+  "data,b16,lr0.0004/03-09-11:53/"
 )
 #mydataset=(
 #  20210308_184153
